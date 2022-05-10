40e281b793c531fd8e797cbc8c54103c896687b7
SafaaFathii <safaafathii123@gmail.com>
2022-05-10 11:55:52 +0200

    Create New Account
    Admin Create new bank account

--- AgileProject.c
+++ AgileProject.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 struct BankAccount
 {
-    char fullName[50];
-    char fullAddress[50];
+    char *fullName;
+    char *fullAddress;
     long nationalID;
     int age;
     long bankAccountID;
-    char guardien[50];
+    char *guardien;
     long gNationalID;
-    char accountStatus[50];
+    char *accountStatus;
     int balance;
-    char password[50];
+    char *password;
     struct BankAccount *next;
 }account;
 
@@ -29,16 +30,26 @@ client createClient(){
 client addClient(client head,char *fullName,char *fullAddress, long nationalID, int age,long bankAccountID,char *guardien,long gNationalID,char *accountStatus,int balance,char *password){
     client temp,p;
     temp = createClient();
-    temp->fullName = fullName;
-    temp->fullAddress = fullAddress;
-    temp->nationalID = nationalID
-    temp->age = age
-    temp->bankAccountID = bankAccountID
-    temp->guardien = guardien
-    temp->gNationalID = gNationalID
-    temp->accountStatus = accountStatus
-    temp->balance = balance
-    temp->password = password
+    temp->fullName =(char*)malloc(sizeof(char)*(strlen(fullName) + 1));
+    strcpy(temp->fullName, fullName);
+    //temp->fullName = fullName;
+    temp->fullAddress =(char*)malloc(sizeof(char)*(strlen(fullAddress) + 1));
+    strcpy(temp->fullAddress, fullAddress);
+    //temp->fullAddress = fullAddress;
+    temp->nationalID = nationalID;
+    temp->age = age;
+    temp->bankAccountID = bankAccountID;
+    temp->guardien =(char*)malloc(sizeof(char)*(strlen(guardien) + 1));
+    strcpy(temp->guardien, guardien);
+    //temp->guardien = guardien;
+    temp->gNationalID = gNationalID;
+    temp->accountStatus =(char*)malloc(sizeof(char)*(strlen(accountStatus) + 1));
+    strcpy(temp->accountStatus, accountStatus);
+    //temp->accountStatus = accountStatus;
+    temp->balance = balance;
+    temp->password =(char*)malloc(sizeof(char)*(strlen(password) + 1));
+    strcpy(temp->password, password);
+    //temp->password = password;
     if(head == NULL){
         head = temp;  
     }
@@ -56,6 +67,8 @@ client addClient(client head,char *fullName,char *fullAddress, long nationalID, 
 int main()
 {
     printf("Add new account");
+    client first = addClient(NULL,"safaaFathii","saadHunnenSt",2025,23,123654,"fathi",15879,"closed",123,"123654");
+	printf(" %d",first->nationalID);
     
 
 
